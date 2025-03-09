6c922f1e65c089da12b0415af6aba3f0a16c23b8
Admin <stiker1503@gmail.com>
2025-03-09 20:06:56 +0300

    создал тесты для функций card_number_generator и transaction_descriptions

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,5 +1,5 @@
 import pytest
-from src.generators import filter_by_currency
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 @pytest.mark.parametrize(
     "transactions, currency_code, expected_length",
@@ -38,3 +38,41 @@ from src.generators import filter_by_currency
 def test_filter_by_currency(transactions, currency_code, expected_length):
     filtered = list(filter_by_currency(transactions, currency_code))
     assert len(filtered) == expected_length
+
+
+def test_transaction_descriptions(transactions):
+    descriptions = list(transaction_descriptions(transactions))
+    expected_descriptions = [
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод с карты на карту",
+        "Перевод организации"
+    ]
+    assert descriptions == expected_descriptions
+
+def test_transaction_descriptions_empty():
+    descriptions = list(transaction_descriptions([]))
+    assert descriptions == []
+
+
+@pytest.mark.parametrize(
+    "start, end, expected_numbers",
+    [
+        (1, 1, ["0000 0000 0000 0001"]),
+        (1, 3, ["0000 0000 0000 0001", "0000 0000 0000 0002", "0000 0000 0000 0003"]),
+        (10, 12, ["0000 0000 0000 0010", "0000 0000 0000 0011", "0000 0000 0000 0012"]),
+        (9999999999999990, 9999999999999992, [
+            "9999 9999 9999 9990",
+            "9999 9999 9999 9991",
+            "9999 9999 9999 9992"
+        ]),
+    ]
+)
+def test_card_number_generator(start, end, expected_numbers):
+    generated_numbers = list(card_number_generator(start, end))
+    assert generated_numbers == expected_numbers
+
+def test_card_number_generator_empty_range():
+    generated_numbers = list(card_number_generator(5, 4))
+    assert generated_numbers == []
