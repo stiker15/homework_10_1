6a9f37829dc8534b182d23d4d212b509218ff3ba
Admin <stiker1503@gmail.com>
2025-03-25 01:30:10 +0300

    создал модуль external_api.py для конвертации валют в рубли

---
+++ src/external_api.py
@@ -0,0 +1,19 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+def convert_to_rub(transaction):
+    """Конвертирует сумму транзакции в рубли."""
+    amount = transaction.get('amount')
+    currency = transaction.get('currency')
+
+    if currency in ['USD', 'EUR']:
+        api_key = os.getenv('API_KEY')
+        response = requests.get(
+            f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}",
+            headers={"apikey": api_key}
+        )
+        if response.status_code == 200:
+            return float(response.json().get('result', amount))
+    return float(amount)
